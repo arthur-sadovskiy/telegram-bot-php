2d9f31490957d04f8f7aa340b02578bc93bd3f64
Arthur Sadovskiy <arthur.sadovskiy@gmail.com>
2017-07-29 23:05:18 +0300

    test commit * also added config file

--- src/app.php
+++ src/app.php
@@ -1,1 +1,31 @@
 <?php
+
+include('../vendor/autoload.php');
+use Telegram\Bot\Api;
+
+$config = parse_ini_file('app_config.ini');
+
+$telegram = new Api($config['token']);
+$result = $telegram->getWebhookUpdates();
+
+$providedText = $result['message']['text'];
+$chatId = $result['message']['chat']['id'];
+$keyboard = [['Weather: Kyiv']];
+
+if (!$providedText) {
+    $replyText = 'Make your choice';
+    $replyMarkup = $telegram->replyKeyboardMarkup(
+        ['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false]
+    );
+
+    $telegram->sendMessage(
+        ['chat_id' => $chatId, 'text' => $replyText, 'reply_markup' => $replyMarkup]
+    );
+
+} elseif ($providedText === 'Weather: Kyiv') {
+    $replyText = '25 С';
+
+    $telegram->sendMessage(
+        ['chat_id' => $chatId, 'text' => $replyText]
+    );
+}
